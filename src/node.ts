712afc340652d29ce0dd3b18490501811c1402ac
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-09-25 13:27:57 +0900

    [dumpJson] 実行環境判定による処理の分岐

--- src/node.ts
+++ src/node.ts
@@ -1,8 +1,16 @@
 import { toJson, fromJson, logJson } from './common'
 
+const isNode = typeof process !== 'undefined' && typeof require !== 'undefined'
+
 const dumpJson = async (filePath: string, data: any) => {
-  const fs = await import('fs-extra')
-  await fs.outputFile(filePath, toJson(data))
+  if (isNode) {
+    const fs = await import('fs-extra')
+    await fs.outputFile(filePath, toJson(data))
+  } else {
+    console.error(
+      '[ERROR in dumpJson (within "json-microscope")] This method is only available in the Node.js execution environment.'
+    )
+  }
 }
 
 export { toJson, fromJson, logJson, dumpJson }
